6001a35e9f196abb122e71f42381457a31f9235a
Ayush Rastogi <ayushr8974@gmail.com>
2026-02-12 17:12:21 -0500

    feature typo

--- backend/stigg/features.js
+++ backend/stigg/features.js
@@ -1,4 +1,4 @@
 // Feature IDs for the different features from Stigg
 export const TEMPLATES_FEATURE_ID = 'feature-01-templates';
 export const CAMPAIGNS_FEATURE_ID = 'feature-02-campaigns';
-export const ANALYTICS_FEATURE_ID = 'feature-03-analytics';
+export const ANALYTICS_FEATURE_ID = 'feature-04-analytics';
